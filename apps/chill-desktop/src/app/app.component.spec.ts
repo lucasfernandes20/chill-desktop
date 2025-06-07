88540c18ab05b547f814fc22f9c444e3dd6b961a
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2025-06-06 21:30:21 -0300

    chore: update dependencies and add Tailwind CSS configuration for chill-desktop

--- apps/chill-desktop/src/app/app.component.spec.ts
+++ apps/chill-desktop/src/app/app.component.spec.ts
@@ -14,9 +14,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome chill-desktop'
-    );
+    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome chill-desktop');
   });
 
   it(`should have as title 'chill-desktop'`, () => {
