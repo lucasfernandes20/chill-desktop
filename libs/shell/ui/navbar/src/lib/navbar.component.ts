517161c6c113eb5f802493d4fba68776648bb606
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2025-06-07 14:01:53 -0300

    feat: customizing angular material theme

--- libs/shell/ui/navbar/src/lib/navbar.component.ts
+++ libs/shell/ui/navbar/src/lib/navbar.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'chill-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
