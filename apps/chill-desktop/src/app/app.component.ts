1a8afc815454eafbc50385427b3cd6c1dae7fc67
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2025-06-07 02:27:15 -0300

    fix: update styleUrl to styleUrls in app.component.ts

--- apps/chill-desktop/src/app/app.component.ts
+++ apps/chill-desktop/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { RouterModule } from '@angular/router';
   imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'chill-desktop';
