962c7ec8b3e745cf8fe0af2ef2f326f68ebc6043
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2025-06-17 23:09:27 -0300

    feat: add Portuguese locale support in app configuration

--- apps/chill-desktop/src/app/app.config.ts
+++ apps/chill-desktop/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { appRoutes } from './app.routes';
@@ -9,6 +9,9 @@ import { reducers, WeatherEffects } from '@chill-desktop/data-acess';
 import { provideHttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment.development';
 import { APP_ENVIRONMENT } from '@chill-desktop/shared/app-environment';
+import { registerLocaleData } from '@angular/common';
+import localePT from '@angular/common/locales/pt';
+registerLocaleData(localePT);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,6 +22,10 @@ export const appConfig: ApplicationConfig = {
     {
       provide: APP_ENVIRONMENT,
       useValue: environment,
+    },
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR',
     },
 
     // NgRx configuration
