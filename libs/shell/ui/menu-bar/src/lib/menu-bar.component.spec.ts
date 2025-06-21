50cc24fc01300bc22feff002113d85653c5866bf
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2025-06-21 04:42:38 -0300

    fix: update loading and error handling for weather and currency components in menu bar

--- libs/shell/ui/menu-bar/src/lib/menu-bar.component.spec.ts
+++ libs/shell/ui/menu-bar/src/lib/menu-bar.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MenuBarComponent } from './menu-bar.component';
 import { StateStatus, TemperatureUnitEnum, WeatherConditionTypeEnum } from '@chill-desktop/shared/models';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
-import { selectWeatherState, type WeatherState } from '@chill-desktop/data-acess';
+import { CurrencyState, selectCurrencyState, selectWeatherState, type WeatherState } from '@chill-desktop/data-acess';
 import { DatePipe } from '@angular/common';
 import { LOCALE_ID } from '@angular/core';
 import { of } from 'rxjs';
@@ -10,31 +10,75 @@ import { of } from 'rxjs';
 const mockWeatherStateLoading: WeatherState = {
   status: StateStatus.LOADING,
   error: undefined,
+  data: {
+    temperature: {
+      degrees: 0,
+      unit: TemperatureUnitEnum.CELSIUS,
+    },
+    feelsLikeTemperature: {
+      degrees: 0,
+      unit: TemperatureUnitEnum.CELSIUS,
+    },
+    weatherCondition: {
+      icon: '',
+      description: {
+        languageCode: '',
+        text: '',
+      },
+      type: WeatherConditionTypeEnum.CLEAR,
+    },
+    isDaytime: true,
+    cloudCover: 0,
+    relativeHumidity: 0,
+    coordinates: {
+      latitude: 0,
+      longitude: 0,
+    },
+    wind: {
+      speed: 0,
+      direction: 0,
+    },
+    visibility: 0,
+    pressure: 0,
+    precipitation: 0,
+    locationName: '',
+  },
 };
 
 const mockWeatherStateSuccess: WeatherState = {
   status: StateStatus.SUCCESS,
   data: {
     temperature: {
-      unit: TemperatureUnitEnum.CELSIUS,
       degrees: 19.13,
+      unit: TemperatureUnitEnum.CELSIUS,
     },
     feelsLikeTemperature: {
-      unit: TemperatureUnitEnum.CELSIUS,
       degrees: 18.77,
+      unit: TemperatureUnitEnum.CELSIUS,
     },
     weatherCondition: {
       icon: 'wb_sunny',
       description: {
-        text: 'céu limpo',
         languageCode: 'pt',
+        text: 'céu limpo',
       },
       type: WeatherConditionTypeEnum.CLEAR,
     },
     isDaytime: false,
-    uvIndex: 0,
     cloudCover: 0.01,
     relativeHumidity: 64,
+    coordinates: {
+      latitude: 19.13,
+      longitude: 18.77,
+    },
+    wind: {
+      speed: 19.13,
+      direction: 18.77,
+    },
+    visibility: 19.13,
+    pressure: 18.77,
+    precipitation: 19.13,
+    locationName: 'São Paulo',
   },
   error: undefined,
 };
@@ -42,6 +86,77 @@ const mockWeatherStateSuccess: WeatherState = {
 const mockWeatherStateError: WeatherState = {
   status: StateStatus.ERROR,
   error: 'Error loading weather',
+  data: {
+    temperature: {
+      degrees: 0,
+      unit: TemperatureUnitEnum.CELSIUS,
+    },
+    feelsLikeTemperature: {
+      degrees: 0,
+      unit: TemperatureUnitEnum.CELSIUS,
+    },
+    weatherCondition: {
+      icon: '',
+      description: {
+        languageCode: '',
+        text: '',
+      },
+      type: WeatherConditionTypeEnum.CLEAR,
+    },
+    isDaytime: true,
+    cloudCover: 0,
+    relativeHumidity: 0,
+    coordinates: {
+      latitude: 0,
+      longitude: 0,
+    },
+    wind: {
+      speed: 0,
+      direction: 0,
+    },
+    visibility: 0,
+    pressure: 0,
+    precipitation: 0,
+    locationName: '',
+  },
+};
+
+const mockCurrencyStateSuccess: CurrencyState = {
+  status: StateStatus.SUCCESS,
+  data: {
+    fromCurrency: 'USD',
+    toCurrency: 'BRL',
+    targetRate: 5.25,
+    rates: {
+      USD: 1,
+      BRL: 5.25,
+    },
+    lastUpdated: new Date(),
+  },
+};
+
+const mockCurrencyStateError: CurrencyState = {
+  status: StateStatus.ERROR,
+  error: 'Error loading currency',
+  data: {
+    fromCurrency: 'USD',
+    toCurrency: 'BRL',
+    targetRate: 0,
+    rates: {},
+    lastUpdated: new Date(),
+  },
+};
+
+const mockCurrencyStateLoading: CurrencyState = {
+  status: StateStatus.LOADING,
+  error: undefined,
+  data: {
+    fromCurrency: 'USD',
+    toCurrency: 'BRL',
+    targetRate: 0,
+    rates: {},
+    lastUpdated: new Date(),
+  },
 };
 
 describe('MenuBarComponent', () => {
@@ -56,7 +171,10 @@ describe('MenuBarComponent', () => {
         DatePipe,
         { provide: LOCALE_ID, useValue: 'pt-BR' },
         provideMockStore({
-          selectors: [{ selector: selectWeatherState, value: mockWeatherStateLoading }],
+          selectors: [
+            { selector: selectWeatherState, value: mockWeatherStateLoading },
+            { selector: selectCurrencyState, value: mockCurrencyStateLoading },
+          ],
         }),
       ],
     }).compileComponents();
@@ -73,7 +191,7 @@ describe('MenuBarComponent', () => {
 
   describe('Weather', () => {
     it('should render loading icon when weather is loading', () => {
-      const loadingIcon = fixture.nativeElement.querySelector('mat-icon[data-testid="loading-icon"]');
+      const loadingIcon = fixture.nativeElement.querySelector('mat-icon[data-testid="weather-loading-icon"]');
       expect(loadingIcon).toBeTruthy();
     });
 
@@ -91,7 +209,7 @@ describe('MenuBarComponent', () => {
       fixture.detectChanges();
       const weatherTemperature = fixture.nativeElement.querySelector('span[data-testid="weather-temperature"]');
       expect(weatherTemperature).toBeTruthy();
-      expect(weatherTemperature.textContent).toBe('19 °C');
+      expect(weatherTemperature.textContent).toBe('19.13 °C');
     });
 
     it('should render error icon when weather is error', () => {
@@ -106,7 +224,7 @@ describe('MenuBarComponent', () => {
       store.overrideSelector(selectWeatherState, mockWeatherStateError);
       store.refreshState();
       fixture.detectChanges();
-      const errorMessage = fixture.nativeElement.querySelector('span[data-testid="error-message"]');
+      const errorMessage = fixture.nativeElement.querySelector('div[data-testid="weather-error"] > span');
       expect(errorMessage).toBeTruthy();
       expect(errorMessage.textContent).toBe('Error loading weather');
     });
@@ -203,17 +321,48 @@ describe('MenuBarComponent', () => {
 
   describe('Currency', () => {
     it('should render currency icon', () => {
+      store.overrideSelector(selectCurrencyState, mockCurrencyStateSuccess);
+      store.refreshState();
+      fixture.detectChanges();
+
       const currencyIcon = fixture.nativeElement.querySelector('mat-icon[data-testid="currency-icon"]');
       expect(currencyIcon).toBeTruthy();
     });
 
     it('should render currency value', () => {
+      store.overrideSelector(selectCurrencyState, mockCurrencyStateSuccess);
+      store.refreshState();
+      fixture.detectChanges();
+
       const currencyValue = fixture.nativeElement.querySelector('span[data-testid="currency-value"]');
       expect(currencyValue).toBeTruthy();
       expect(currencyValue.textContent).toBe('1 USD = 5.25 BRL');
     });
 
+    it('should render an error message when currency is error', () => {
+      store.overrideSelector(selectCurrencyState, mockCurrencyStateError);
+      store.refreshState();
+      fixture.detectChanges();
+
+      const currencyError = fixture.nativeElement.querySelector('div[data-testid="currency-error"] > span');
+      expect(currencyError).toBeTruthy();
+      expect(currencyError.textContent).toBe('Error loading currency');
+    });
+
+    it('should render a loading icon when currency is loading', () => {
+      store.overrideSelector(selectCurrencyState, mockCurrencyStateLoading);
+      store.refreshState();
+      fixture.detectChanges();
+
+      const loadingIcon = fixture.nativeElement.querySelector('mat-icon[data-testid="currency-loading-icon"]');
+      expect(loadingIcon).toBeTruthy();
+    });
+
     it('should open a mat-menu when currency button is clicked', () => {
+      store.overrideSelector(selectCurrencyState, mockCurrencyStateSuccess);
+      store.refreshState();
+      fixture.detectChanges();
+
       const currencyButton = fixture.nativeElement.querySelector('button[data-testid="currency-button"]');
       currencyButton.click();
       fixture.detectChanges();
